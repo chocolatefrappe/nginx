f23882354075410c6de2cea79dba9e034ccfa3f8
Socheat Sok <socheatsok78@gmail.com>
2023-07-12 03:19:18 +0700

    Improve 40-generate-dhparam.sh log output

--- rootfs/docker-entrypoint.d/40-generate-dhparam.sh
+++ rootfs/docker-entrypoint.d/40-generate-dhparam.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ME=$(basename "${0}")
+
 # Generate dhparam.pem file if it does not exist
 # Default key bits is 1024, but 2048 or 4096 is recommended
 
@@ -7,6 +9,8 @@ DHPARAM_FILE="${DHPARAM_FILE:-/etc/nginx/dhparam.pem}"
 DHPARAM_KEY_BITS=${DHPARAM_KEY_BITS:-2048}
 
 if [ ! -f "${DHPARAM_FILE}" ]; then
-    echo -n "[-] Generating DH parameters, ${DHPARAM_KEY_BITS} bit long safe prime"
+    echo -n "$ME: Generating DH parameters, ${DHPARAM_KEY_BITS} bit long safe prime..."
     openssl dhparam -out "${DHPARAM_FILE}" ${DHPARAM_KEY_BITS} 2> /dev/null
+else
+    echo "$ME: Using existing DH parameters!"
 fi
