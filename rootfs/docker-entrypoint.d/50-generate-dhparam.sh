3ca4036b01736913193f779e1fd40f73dcc89055
Socheat Sok <socheatsok78@gmail.com>
2023-07-12 18:19:26 +0700

    Update `50-generate-dhparam.sh` script

--- rootfs/docker-entrypoint.d/50-generate-dhparam.sh
+++ rootfs/docker-entrypoint.d/50-generate-dhparam.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
+set -e
 
 ME=$(basename "${0}")
+SSL_DHPARAM_ENABLED=${SSL_DHPARAM_ENABLED:-true}
+
+if [ "${SSL_DHPARAM_ENABLED}" != "true" ]; then
+    entrypoint_log "$ME: info: SSL_DHPARAM_ENABLED is not true, skipping"
+    exit 0
+fi
 
 # Generate dhparam.pem file if it does not exist
 # Default key bits is 1024, but 2048 or 4096 is recommended
 
-DHPARAM_FILE="${DHPARAM_FILE:-/etc/nginx/dhparam.pem}"
-DHPARAM_KEY_BITS=${DHPARAM_KEY_BITS:-2048}
+SSL_DHPARAM_FILE="${SSL_DHPARAM_FILE:-/etc/nginx/dhparam.pem}"
+SSL_DHPARAM_KEY_BITS=${SSL_DHPARAM_KEY_BITS:-2048}
 
-echo -n "$ME: Generating DH parameters, ${DHPARAM_KEY_BITS} bit long safe prime..."
-if [ ! -f "${DHPARAM_FILE}" ]; then
-    openssl dhparam -out "${DHPARAM_FILE}" ${DHPARAM_KEY_BITS} 2> /dev/null
-    if [ $? -ne 0 ]; then
-        echo " [ERROR]"
-        exit 1
-    fi
-    echo " [DONE]"
-else
-    echo " [SKIP]"
+echo "$ME: Generating DH parameters, ${SSL_DHPARAM_KEY_BITS} bit long safe prime. This will take some time..."
+if [ ! -f "${SSL_DHPARAM_FILE}" ]; then
+    openssl dhparam -out "${SSL_DHPARAM_FILE}" ${SSL_DHPARAM_KEY_BITS} 2> /dev/null
 fi
