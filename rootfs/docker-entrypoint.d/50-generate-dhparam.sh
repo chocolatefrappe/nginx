68382cab1f5b2e4a7d0f95dfb1d2547ee6bfadfd
Socheat Sok <socheatsok78@gmail.com>
2023-07-12 17:35:49 +0700

    exit 1 if 50-generate-dhparam.sh failed

--- rootfs/docker-entrypoint.d/50-generate-dhparam.sh
+++ rootfs/docker-entrypoint.d/50-generate-dhparam.sh
@@ -11,6 +11,10 @@ DHPARAM_KEY_BITS=${DHPARAM_KEY_BITS:-2048}
 echo -n "$ME: Generating DH parameters, ${DHPARAM_KEY_BITS} bit long safe prime..."
 if [ ! -f "${DHPARAM_FILE}" ]; then
     openssl dhparam -out "${DHPARAM_FILE}" ${DHPARAM_KEY_BITS} 2> /dev/null
+    if [ $? -ne 0 ]; then
+        echo " [ERROR]"
+        exit 1
+    fi
     echo " [DONE]"
 else
     echo " [SKIP]"
