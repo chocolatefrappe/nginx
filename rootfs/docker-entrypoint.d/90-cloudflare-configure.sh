be533c7663915578ebc8f873c0491d85d81ba359
Socheat Sok <socheatsok78@gmail.com>
2024-09-27 14:46:46 +0700

    Rework Cloudflare config

---
+++ rootfs/docker-entrypoint.d/90-cloudflare-configure.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+
+entrypoint_log() {
+    if [ -z "${NGINX_ENTRYPOINT_QUIET_LOGS:-}" ]; then
+        echo "$@"
+    fi
+}
+
+ME=$(basename "${0}")
+
+if [ ${CLOUDFLARE_ENABLED:-false} == true ]; then
+    if [ ! -f "/etc/nginx/conf.d/cloudflare.conf" ]; then
+        entrypoint_log "${ME}: Enabling Cloudflare configuration"
+        ln -s /etc/nginx/conf-available.d/cloudflare.conf  /etc/nginx/conf.d/cloudflare.conf
+    fi
+fi
